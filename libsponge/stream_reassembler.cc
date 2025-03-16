47e524d5981f0e19d3f2b73622441746af23d803
linux <smalldogp625@gmail.com>
2025-03-17 01:38:12 +0800

    lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -19,8 +19,129 @@ StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity),
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
     DUMMY_CODE(data, index, eof);
+    //传入的数据三种情况构成数据块
+    //index>capacity+_head_index; [index                                 ]
+    
+    //1：包含关系;
+    //2: [                              _head_index]
+    //               [index                ]
+    //3:[       ]
+    if(index>=_capacity+_head_index)
+    {
+        return;//越界了，无法接受数据
+    }
+    block_note elm;
+    //构建数据块
+    if(index+data.length()<=_head_index)
+    {
+        goto JUDGE_EOF;
+    }
+    else if(index<_head_index)
+    {
+        size_t offset = _head_index-index;
+        
+        elm.data.assign(data.begin()+offset,data.end());
+        elm.begin = index + offset;
+        elm.length = elm.data.length(); 
+    }
+    else{
+        elm.begin = index;
+        elm.data = data;
+        elm.length = data.length();
+    }
+    unassembled_byte_num +=elm.length; //未重组字节数
+
+
+     // merge substring
+     do{
+        //合并_block中elem和ta之后的数据块
+        long merged_bytes = 0;
+        auto iter = _blocks.lower_bound(elm);
+
+        while(iter!=_blocks.end()&&(merged_bytes=merge_block(elm,*iter))>=0)
+        {
+            unassembled_byte_num -=merged_bytes;
+            _blocks.erase(iter);
+            iter = _blocks.lower_bound(elm);
+        }
+        if(iter == _blocks.begin())
+        {
+            break;
+        }
+        //合并_block中elem和之前的数据块
+        iter--;
+        while((merged_bytes=merge_block(elm,*iter))>=0)
+        {
+            unassembled_byte_num-=merged_bytes;
+            _blocks.erase(iter);
+            iter = _blocks.lower_bound(elm);
+            if(iter ==_blocks.begin())
+            {
+                break;
+            }
+            iter --;
+
+        }
+
+     }while(false);
+     _blocks.insert(elm);
+     //合并数据块之后，加入到out_put中去
+    //_block_index:{0,1,2,3,...}
+    //_output.write: return 传入的字节数，比如传入2字节，那么_head_index 为0+2 =2;
+    //_head_index:下一个要传入的index
+    if(!_blocks.empty()&&_blocks.begin()->begin == _head_index)
+    {
+        const block_note head_node = *_blocks.begin();
+        size_t wirtten_bytes = _output.write(head_node.data);
+
+        _head_index+=wirtten_bytes;
+        unassembled_byte_num-=wirtten_bytes;
+        _blocks.erase(_blocks.begin());
+
+    }
+
+    JUDGE_EOF:
+    if (eof) {
+        _eof_flag = true;
+    }
+    if (_eof_flag && empty()) {
+        _output.end_input();
+    }
 }
+//! elm：传入的数据块，1代表合并后的数据块
+//！return 合并增加的数据长度 失败返回-1；
+long StreamReassembler::merge_block(block_note &elm1 ,const block_note &elm2) //
+{
+    block_note x,y;
+    //选取X作为第一个元素下表
+    if(elm1.begin>elm2.begin)
+    {
+        x = elm2;
+        y = elm1;
+    }
+    else
+    {
+        x = elm1;
+        y = elm2;
+    }
+    if(x.begin+x.length<y.begin)
+    {
+        return -1; //合并失败
+    }
+    else if(x.begin+x.length>y.begin+y.length)
+    {
+        elm1 =x;
+        return y.length;
+    }
+    else{
+        elm1.begin = x.begin;
+        elm1.data = x.data+y.data.substr(x.begin+x.length-y.begin);
+        elm1.length = elm1.data.length();
+        return x.begin+x.length-y.begin;
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+    }
 
-bool StreamReassembler::empty() const { return {}; }
+}
+size_t StreamReassembler::unassembled_bytes() const { return {unassembled_byte_num}; }
+
+bool StreamReassembler::empty() const { return {unassembled_byte_num==0}; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,21 +5,32 @@
 
 #include <cstdint>
 #include <string>
-
+#include<set>
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
+    struct block_note{
+      size_t begin =0;
+      size_t length =0;
+      std::string data = "";
+      bool operator<(const block_note t) const { return begin < t.begin; }
+    };
 
+    size_t unassembled_byte_num = 0;
+    size_t _head_index = 0;
+    bool _eof_flag=false;
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
-
+    std::set<block_note> _blocks={};
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
     //! \note This capacity limits both the bytes that have been reassembled,
     //! and those that have not yet been reassembled.
     StreamReassembler(const size_t capacity);
+    //合并两个segment
+    long merge_block(block_note &elm1 ,const block_note &elm2);
 
     //! \brief Receive a substring and write any newly contiguous bytes into the stream.
     //!
